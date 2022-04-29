fd1b06e5fd98365efdcc8bcf65bdb2f5aadf51b2
JiawenZhuo <jiawen.zhuo@uq.net.au>
2022-04-29 22:06:04 +1000

    change repo

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+
+const dbo = require('./conn');
+
+
+app.use(cors());
+app.use(express.json());
+
+const recordRoutes = require('./routes/records');
+app.use(recordRoutes);
+
+
+  dbo.connectToServer(function (err) {
+    if (err) {
+      console.error(err);
+      process.exit();
+    }
+
+    app.listen(port, () =>{
+        console.log(`example app listen on port ${port}`);
+    });
+  });
+
+
